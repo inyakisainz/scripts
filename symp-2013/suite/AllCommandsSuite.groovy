dbd58868b5dc79302228a5155812b86f88315178
Inyaki Sainz <inyaki.sainz@liferay.com>
2013-11-20 11:11:51 +0100

    Añadir versión inicial

---
+++ symp-2013/suite/AllCommandsSuite.groovy
@@ -0,0 +1,20 @@
+package suite
+
+import core.builders.CommandBuilder
+import core.resulthandlers.LiferayConsoleOutputHandler;
+import com.liferay.portal.kernel.cluster.ClusterExecutorUtil;
+
+out.print("Starting...");
+
+// Dynamic determine if runnen on cluster or not
+boolean isCluster=ClusterExecutorUtil.isEnabled();
+
+cb = new CommandBuilder("https://raw.github.com/inyakisainz/scripts/master/symp-2013/", isCluster);
+cb.appendCommand("commands/MemoryUsageCommand.groovy", "MemoryUsageCommand");
+cb.appendCommand("commands/GetPortalImplManifestAttrsCommand.groovy", "GetPortalImplManifestAttrsCommand");
+cb.appendCommand("commands/GetMachineMemoryCommand.groovy", "GetMachineMemoryCommand");
+cb.appendCommand("commands/GetJVMInfoCommand.groovy", "GetJVMInfoCommand");
+cb.appendCommand("commands/GetCPUInfoCommand.groovy", "GetCPUInfoCommand");
+cb.start(new LiferayConsoleOutputHandler(out));
+
+
